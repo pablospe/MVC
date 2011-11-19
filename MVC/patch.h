24eecb49ccc83aef9ae047532e48c8800cc196f5
Julius Elinson <juliuselinson@yahoo.com>
2011-11-19 13:48:12 -0800

    framework for bresenham alg

--- MVC/patch.h
+++ MVC/patch.h
@@ -12,7 +12,8 @@ public:
 	Patch(int width, int height);
 	bool addPoint(Point& vertex);
 	void computeInterior();
-	void linearize(Point pt1, Point pt2, vector<Point>& border );
+	void fillBoundary();
+	void linearize(Point pt1, Point pt2, vector<Point>& border);
 	void highLight(Image* img) const;
 
 	vector<Point> interior;
